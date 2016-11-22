9881aa5b197f910be3a01942c7d4dd2fd6f8ae4a
Vladimir Terenin <vladimirterenin@MacBook-Vladimir.local>
2016-11-22 20:20:12 +0300

    Initial Commit

---
+++ Deitel.Chapter1.C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Deitel.Chapter1.C
+//
+//  Created by Vladimir Terenin on 22.11.16.
+//  Copyright © 2016 Vladimir Terenin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
